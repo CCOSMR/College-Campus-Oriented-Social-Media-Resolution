2d316064c6c353869f21a158a9d74d4bdfb741ec
SymPy <568603742@qq.com>
2018-03-21 14:00:04 +0800

    New functions! (#2)
    Sign up and Login!

---
+++ func/database.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+
+class Database:
+    def __init__(self, database_name: str):
+        self.name = database_name
+
+    def simple_search(self, table_name: str, condition: str):
+        conn = sqlite3.connect(self.name)
+        c = conn.cursor()
+        operation = "select * from {} where {}".format(table_name, condition)
+        print(operation)
+        data = c.execute(operation).fetchall()
+        conn.commit()
+        conn.close()
+        return data
+
+    def add_new_user(self, data):
+        conn = sqlite3.connect(self.name)
+        c = conn.cursor()
+        table_name = "user_table"
+        headers = "id, password, email, name, time_joined"
+        judge = lambda x: False if isinstance(x, str) else True
+        quotation = lambda x: "\"" + x + "\""
+        values = ", ".join([str(i) if judge(i) else quotation(i) for i in data.values()]) + ", DATETIME(\"now\")"
+        operation = "INSERT INTO {} ({}) VALUES ({})".format(table_name, headers, values)
+        print(operation)
+        c.execute(operation)
+        conn.commit()
+        conn.close()
+
+    def insert(self, table_name: str, data: dict):
+        conn = sqlite3.connect(self.name)
+        c = conn.cursor()
+        headers = ", ".join(data.keys())
+        judge = lambda x: False if isinstance(x, str) else True
+        quotation = lambda x: "\"" + x + "\""
+        values = ", ".join([str(i) if judge(i) else quotation(i) for i in data.values()])
+        operation = "INSERT INTO {} ({}) VALUES ({})".format(table_name, headers, values)
+        print(operation)
+        c.execute(operation)
+        conn.commit()
+        conn.close()
---
+++ func/server.py
@@ -0,0 +1,36 @@
+import re
+import flask
+import time
+import json
+from func import verified_code
+
+
+def signup(database, data: dict):
+    if database.simple_search("user_table", "id = \"{}\"".format(data["id"])):
+        return 101
+    if data["password"] != data["confirm"]:
+        return 102
+    if len(data["email"]) > 7:
+        pattern = "^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$"
+        if not re.match(pattern, data["email"]):
+            return 104
+    # if data["verifiedcode"]:
+    data = {
+        "id": data["id"],
+        "password": data["password"],
+        "email": data["email"],
+        "nickname": data["nickname"],
+    }
+    database.add_new_user(data)
+
+
+def signin(database, data: dict):
+    search_re = database.simple_search("user_table", "id=\"{}\"".format(data["id"]))
+    if search_re:
+        id, password, email, time_joined, _, nickname, _ = search_re[0]
+    else:
+        return 101
+    if password == data["password"]:
+        return 200
+    else:
+        return 101
